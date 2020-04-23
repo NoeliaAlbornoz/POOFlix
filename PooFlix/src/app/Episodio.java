2f52dce7da97c8e263681dfd3fdea31531de1303
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-04-23 10:33:15 -0300

    Constructor de 3 parámetros.

--- PooFlix/src/app/Episodio.java
+++ PooFlix/src/app/Episodio.java
@@ -2,10 +2,44 @@ package app;
 
 public class Episodio {
 
+    // Constructor de 3 parametros
+    // Acceso: public
+    // Nombre de la clase q se quiere instanciar
+    // Parametro pueden tener cualquier orden y nombre
+    public Episodio(int numero, String nombre, int duracion) {
+        this.numero = numero;
+        this.nombre = nombre;
+        this.duracionEnMinutos = duracion;
+    }
+
     public String nombre;
     public int numero;
     public String descripcion;
     public int duracionEnMinutos;
+
+    // Constructor por defecto - vacio (viene por defecto - no se ve), pero si creo
+    // uno, debo crear a mano el vacio si lo necesito
+    //public Episodio() {}
+
+    //NOOOO hacer constructor para inicializar todas las variables
+    /*public Episodio(int numero, String nombre, int duracion, String descripcion) {
+        this.numero = numero;
+        this.nombre = nombre;
+        this.duracionEnMinutos = duracion;
+        this.descripcion = descripcion;
+    }*/
+
+    /*No vale la pena
+    public Episodio(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    No vale la pena
+    public Episodio(String descripcion, int numero) {
+        this.descripcion = descripcion;
+        this.numero = numero;
+    }*/
 
     public void reproducir() {
 
